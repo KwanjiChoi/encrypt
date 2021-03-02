ac815540cde54726b4e99669f25d84feeb4e67fc
KwanjiChoi <kwanji0912@gmail.com>
2021-03-03 00:27:18 +0900

    edit ARGV

--- caesar.rb
+++ caesar.rb
@@ -1,22 +1,17 @@
-puts "キーワードを入力してください（アルファベット小文字のみ）"
-keyword = gets.chomp
-
+file = open(ARGV[0])
 puts "暗号キーを入力してください(数字)"
-num = gets.to_i
-
-puts "暗号化前: #{keyword}"
+num = STDIN.gets.to_i
+content = file.read
 
 def encryption(str, key)
   code = []
   str.chars.each do |char|
-    num = char.ord - 97
-    num2 = (num - key) % 26
-    num3 = num2 + 97
-    code << num3.chr
+    encrypted_char = char.ord - key
+    code << encrypted_char.chr
   end
   code.join('')
 end
 
-result = encryption(keyword, num)
-
+result = encryption(content, num)
 puts "暗号化後: #{result}"
+
--- sample.rb
+++ sample.rb
@@ -1,5 +1,4 @@
-ary  = [*'a'..'z']
-
-ary.each do |char|
-  puts char.ord
-end
+file = open(ARGV[0])
+puts "暗号キーを入力してください"
+ARGV.clear
+num = gets.to_i
